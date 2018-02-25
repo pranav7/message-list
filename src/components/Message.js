d0092804f3ae7ed0fee0aa264e1c02b691cb8e3d
Pranav Singh <pranav@supportbee.com>
2018-02-25 22:02:43 +0530

    Fix fix fix

--- src/components/Message.js
+++ src/components/Message.js
@@ -37,7 +37,7 @@ class Message extends Component {
     });
 
     hammer.on("panend", (event) => {
-      if(event.deltaX > 200 || event.deltaX < -200) {
+      if(Math.abs(event.deltaX) >= 200) {
         messageEl.style.transform = `translate3d(${event.deltaX * 3}px, 0, 0)`;
 
         $(messageEl).animate({
